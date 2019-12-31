4463b0ff800e76eec60fb94a8f1a43b608cb77d7
wangpu <wp_def@163.com>
2019-12-31 18:04:53 +0800

    手机提交页优化

--- newyear/views.py
+++ newyear/views.py
@@ -33,28 +33,35 @@ class newYear(View):
         con = {"cate":cate}
         return render(request, "newyear/index.html", con)
 
-    def post(self,request):
-        """报名提交页"""
-        print(request)
-        data = {}
-        data["name"] = request.POST.get('name',None)
-        data["phone"] = request.POST.get('phone',None)
-        user = User(**data)
-        user.save()
-        return HttpResponse("提交成功")
+    # def post(self,request):
+    #     """报名提交页"""
+    #     print(request)
+    #     data = {}
+    #     data["name"] = request.POST.get('name',None)
+    #     data["phone"] = request.POST.get('phone',None)
+    #     user = User(**data)
+    #     user.save()
+    #     con={"color":"red","res_title":""}
+    #     return render(request, "newyear/res.html", con)
 
 class User(View):
     """用户等级"""
     def get(self,request):
+        """报名页展示"""
         return render(request,"newyear/user.html")
+
     def post(self,request):
+        """报名页内容提交"""
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         ret = re.match(r"^1[35678]\d{9}$", phone)
         if ret is None:
-            return HttpResponse("请输入正确的手机号")
+            con = {"color": "red", "res_title": "提交失败","res_info":"请输入正确的手机号"}
+            return render(request,"newyear/res.html",con)
         user = UserInfo.objects.filter(phone=phone)
         if user:
-            return HttpResponse("你已经注册")
+            con = {"color": "red", "res_title": "提交失败", "res_info": "该手机号已经注册"}
+            return render(request, "newyear/res.html", con)
         UserInfo.objects.create(name=name,phone=phone)
-        return HttpResponse("尊敬的%s 先生/女士，您的信息已成功登记,请等待抽奖"%(name))
+        con = {"color": "green", "res_title": "提交失败", "res_info": "尊敬的%s 先生/女士，您的信息已成功登记,请等待抽奖"%(name)}
+        return render(request, "newyear/res.html", con)
